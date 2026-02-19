7d9704ae78121064cf8d74452a75b9df4b3323a4
Andrey Senkovich <senkovich@gmail.com>
2026-02-19 11:17:28 +0300

    Init Commit

---
+++ src/test/java/ru/netology/javaqa/TaskTest.java
@@ -0,0 +1,79 @@
+package ru.netology.javaqa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TaskTest {
+
+    @Test
+    public void shouldMatchesSimpleTaskQueryFound() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        boolean expected = true;
+        boolean actual = simpleTask.matches(("родителям"));
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldMatchesSimpleTaskQueryNotFound() {
+        SimpleTask simpleTask = new SimpleTask(6, "Позвонить маме");
+
+        boolean expected = false;
+        boolean actual = simpleTask.matches(("родителям"));
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldMatchesEpicQueryFound() {
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        Epic epic = new Epic(55, subtasks);
+
+        boolean expected = true;
+        boolean actual = epic.matches(("Хлеб"));
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldMatchesEpicQueryNotFound() {
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        Epic epic = new Epic(56, subtasks);
+
+        boolean expected = false;
+        boolean actual = epic.matches(("Картошка"));
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldMatchesMeetingQueryFound() {
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        boolean expected = true;
+        boolean actual = meeting.matches(("НетоБанка"));
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldMatchesMeetingQueryNotFound() {
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        boolean expected = false;
+        boolean actual = meeting.matches(("4й версии"));
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
